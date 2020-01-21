7456e70ca03172c1d93d4d9472949aa47eaaa699
Itiel Shwartz <itielshw@gmail.com>
2020-01-21 18:46:48 +0200

    add endpoint

--- server.py
+++ server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
+@app.route('/hello-world-2')
+def hello_two():
+    return "hello world-2"
+
 
 app.run(debug=True, host='0.0.0.0', port=5000)
